980bfec265db04e6e39256e0d5e43a73cf2bb040
VooDooN735 <107447207+VooDooN735@users.noreply.github.com>
2022-07-07 23:09:46 +0300

    Bonus

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,14 +1,14 @@
 public class Bonus {
     public static void main(String[] args) {
         int balance = 100;
-        int addBalance = 10;
+        int addBalance = 101;
         int bonus = addBalance / 100;
 
 
         if (addBalance > 1000) {
             System.out.println("Бонус: " + bonus + "р" + " Баланс: " + (balance + addBalance + bonus) + "р");
         } else {
-            System.out.println("Бонус: " + bonus + "р" + " Баланс: " + (balance + addBalance) + "р");
+            System.out.println("Бонус: " + 0 + "р" + " Баланс: " + (balance + addBalance) + "р");
         }
 
     }
